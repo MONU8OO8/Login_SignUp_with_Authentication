a91a3334f836494db91d822172b508df5ec128a8
MONU8OO8 <monu270800@gmail.com>
2023-03-28 02:49:38 +0530

    creating pass

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -122,7 +122,7 @@ export async function login(req,res){
                         const token = jwt.sign({
                                         userId: user._id,
                                         username : user.username
-                                    }, ENV.JWT_SECRET , { expiresIn : "24h"});
+                                    }, process.env.JWT_SECRET , { expiresIn : "24h"});
 
                         return res.status(200).send({
                             msg: "Login Successful...!",
