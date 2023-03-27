d9b7f12e230b6885db262fcb0814566bfb857fc9
MONU8OO8 <monu270800@gmail.com>
2023-03-28 03:06:32 +0530

    creating vercel

---
+++ client/config.js
@@ -0,0 +1,8 @@
+export default{
+    // for generate this run this=  openssl rand -base64 32
+    JWT_SECRET : "99faebHcWyKmYoiW0JcDqmpXDzdu2irf6KkQ7YUqNPs=",
+    ATLAS_URI : "mongodb+srv://Monu:Lucifer%40666@mongo.dajlsqt.mongodb.net/loginsignup?authSource=mongo&authMechanism=SCRAM-SHA-1",
+    EMAIL : "mona.flatley49@ethereal.email",
+    PASSWORD :"wMeADWDzF2zKPpyVjz",
+    email: "lucifer8u8@gmail.com"
+}
